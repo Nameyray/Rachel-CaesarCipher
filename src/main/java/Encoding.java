0a8ac7b706e11d84f744a44c44c4a132fa3fc579
Nameyray <eparis@atlassian.com>
2022-04-24 22:19:19 +0300

    Generated the getter and setters for the main java encoding file

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -2,6 +2,23 @@ public class Encoding {
     Character[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
     private int shiftkey;
+
+    public int getShiftkey() {
+        return shiftkey;
+    }
+
+    public void setShiftkey(int shiftkey) {
+        this.shiftkey = shiftkey;
+    }
+
+    public String getEncodedText() {
+        return encodedText;
+    }
+
+    public void setEncodedText(String encodedText) {
+        this.encodedText = encodedText;
+    }
+
     private String encodedText;
 }
 
