464cb0483bfb2913a9cbde0ac9afe748ef4333fb
Nameyray <eparis@atlassian.com>
2022-04-24 22:22:14 +0300

    Generated a getter and setters for the Decoding java main file

--- src/main/java/Decoding.java
+++ src/main/java/Decoding.java
@@ -1,5 +1,23 @@
 public class Decoding {
     Character[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
+    private int shiftkey;
 
+    public int getShiftkey() {
+        return shiftkey;
+    }
+
+    public void setShiftkey(int shiftkey) {
+        this.shiftkey = shiftkey;
+    }
+
+    private String mText;
+
+    public String getmText() {
+        return mText;
+    }
+
+    public void setmText(String mText) {
+        this.mText = mText;
+    }
 }
