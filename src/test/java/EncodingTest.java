c98ec824e027fca27ac640f10bf628d850a522a4
Nameyray <eparis@atlassian.com>
2022-04-24 23:04:40 +0300

    generated a getter and setter for the encoding test file

--- src/test/java/EncodingTest.java
+++ src/test/java/EncodingTest.java
@@ -1,4 +1,12 @@
 public class EncodingTest {
     private int shiftKey;
-    private String encodedKey;
+    private String encodedText;
+
+    public String getEncodedText() {
+        return encodedText;
+    }
+
+    public void setEncodedKey(String encodedText) {
+        this.encodedText = encodedText;
+    }
 }
