a9a03d0d30e4c62809a2901bbd69f624211b9f26
Nameyray <eparis@atlassian.com>
2022-04-24 22:17:50 +0300

    Defined the private properties to be used in the app

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,5 +1,7 @@
 public class Encoding {
     Character[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
-
+    private int shiftkey;
+    private String encodedText;
 }
+
