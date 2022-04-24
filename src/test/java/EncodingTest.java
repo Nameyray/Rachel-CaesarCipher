3545ca1451fcb3ff17cdd38172eef7127aacbdff
Nameyray <eparis@atlassian.com>
2022-04-24 23:06:29 +0300

    generated a getter and setter for the encoding shiftKey

--- src/test/java/EncodingTest.java
+++ src/test/java/EncodingTest.java
@@ -1,5 +1,14 @@
 public class EncodingTest {
     private int shiftKey;
+
+    public int getShiftKey() {
+        return shiftKey;
+    }
+
+    public void setShiftKey(int shiftKey) {
+        this.shiftKey = shiftKey;
+    }
+
     private String encodedText;
 
     public String getEncodedText() {
