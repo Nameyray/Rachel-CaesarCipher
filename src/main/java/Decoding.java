4570f2183b09c884efe34fb8c1b73ba2591b7b77
Nameyray <eparis@atlassian.com>
2022-04-24 22:15:12 +0300

    Defined the alphabet characters on the main encoding and decoding file

--- src/main/java/Decoding.java
+++ src/main/java/Decoding.java
@@ -1,4 +1,4 @@
-public class Encoding {
+public class Decoding {
     Character[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
 
